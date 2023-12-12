96189b202bc0d0f9560e0213f360c0d3a49a6e18
Miki_Tellurium <michelegamer14@gmail.com>
2023-12-12 21:31:57 +0100

    Adapted example configs to new changes

--- fabric/src/main/java/com/mikitellurium/telluriumconfigexample/config/ExampleConfig.java
+++ fabric/src/main/java/com/mikitellurium/telluriumconfigexample/config/ExampleConfig.java
@@ -3,11 +3,9 @@ package com.mikitellurium.telluriumconfigexample.config;
 import com.mikitellurium.telluriumconfigexample.TelluriumConfigExampleMod;
 import com.mikitellurium.telluriumconfigexample.api.TelluriumConfig;
 
-import java.io.IOException;
-
 public class ExampleConfig {
     // Create a new config builder
-    public static TelluriumConfig.Builder CONFIG = new TelluriumConfig.Builder(TelluriumConfigExampleMod.MOD_ID);
+    public static TelluriumConfig CONFIG = new TelluriumConfig(TelluriumConfigExampleMod.MOD_ID);
 
     // Create some configs
     public static TelluriumConfig.ConfigEntry<Integer> INT_CONFIG;
@@ -25,21 +23,27 @@ public class ExampleConfig {
                 .comment("This is an example on how to use the TelluriumConfig class");
 
         // Define the configs, the comment is optional
-        INT_CONFIG = CONFIG.define("intConfig", 20);
+        INT_CONFIG = CONFIG.entryBuilder()
+                .define("intConfig", 20);
 
-        STRING_CONFIG = CONFIG.define("stringConfig", "Hello Minecraft!");
+        STRING_CONFIG = CONFIG.entryBuilder()
+                .define("stringConfig", "Hello Minecraft!");
 
-        BOOLEAN_CONFIG = CONFIG.define("booleanConfig", true)
-                .comment("Is this true or false?");
+        BOOLEAN_CONFIG = CONFIG.entryBuilder()
+                .comment("Is this true or false?")
+                .define("booleanConfig", true);
 
-        INT_RANGED_CONFIG = CONFIG.defineInRange("intRangedConfig", 5, 15, 10)
-                .comment("This integer is always inside this range");
+        INT_RANGED_CONFIG = CONFIG.entryBuilder()
+                .comment("This integer is always inside this range")
+                .defineInRange("intRangedConfig", 5, 15, 10);
 
-        DOUBLE_RANGED_CONFIG = CONFIG.defineInRange("doubleRangedConfig", 1.0D, 2.0D, 1.2D)
-                .comment("This double is always inside this range");
+        DOUBLE_RANGED_CONFIG = CONFIG.entryBuilder()
+                .comment("This double is always inside this range")
+                .defineInRange("doubleRangedConfig", 1.0D, 2.0D, 1.2D);
 
-        LONG_RANGED_CONFIG = CONFIG.defineInRange("longRangedConfig", 10000L, 100000L, 25000L)
-                .comment("This long is always inside this range");
+        LONG_RANGED_CONFIG = CONFIG.entryBuilder()
+                .comment("This long is always inside this range")
+                .defineInRange("longRangedConfig", 10000L, 100000L, 25000L);
 
         // Build our config file, always do this for last
         CONFIG.build();
